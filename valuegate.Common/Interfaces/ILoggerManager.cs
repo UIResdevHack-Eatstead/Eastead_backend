07d834b116c748dfea8580b36b9774fc5cc5ccb5
donkennie <ajeigbekehinde160@gmail.com>
2023-08-16 06:10:29 +0100

    remove the default logging

---
+++ valuegate.Common/Interfaces/ILoggerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valuegate.Common.Interfaces
+{
+    public interface ILoggerManager
+    {
+        void LogError(string logEvent, object logData);
+        void LogInfo(string logEvent, object logData);
+    }
+}
--- valuegate.Infrastructure/Logging/LoggerManager.cs
+++ valuegate.Infrastructure/Logging/LoggerManager.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valuegate.Common.Interfaces;
 
 namespace Valuegate.Infrastructure.Logging
 {
-    internal class LoggerManager
+    public class LoggerManager : ILoggerManager
     {
+        public void LogError(string logEvent, object logData)
+        {
+            Log.Error($"Error :: {DateTime.UtcNow}, LogEvent : {logEvent}, LogData : {logData}");
+        }
+
+        public void LogInfo(string logEvent, object logData)
+        {
+            Log.Information($"INFO :: {DateTime.UtcNow}, LogEvent : {logEvent}, LogData : {logData}");
+        }
     }
 }
