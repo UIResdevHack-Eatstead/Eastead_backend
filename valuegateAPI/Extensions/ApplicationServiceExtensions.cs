ca9e0ca525e282936d32a2c3b7e11d5a85034633
donkennie <ajeigbekehinde160@gmail.com>
2023-08-16 06:44:44 +0100

    connect to postgres database

--- valuegateAPI/Extensions/ApplicationServiceExtensions.cs
+++ valuegateAPI/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,44 @@
-﻿namespace Valuegate.API.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using Valuegate.Infrastructure.Data;
+
+namespace Valuegate.API.Extensions
 {
-    public class ApplicationServiceExtensions
+    public static class ApplicationServiceExtension
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration _config)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(Options =>
+            {
+                Options.UseNpgsql(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("X-Pagination"));
+            });
+
+
+            return services;
+        }
+
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage,
+         int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new
+            {
+                currentPage,
+                itemsPerPage,
+                totalItems,
+                totalPages
+            };
+            response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationHeader));
+
+        }
     }
 }
