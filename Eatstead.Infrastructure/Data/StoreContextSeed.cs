de54d64b6e49064053cc73a9483778fb4321a8f2
donkennie <ajeigbekehinde160@gmail.com>
2023-10-10 11:10:25 +0100

    add seed factory mechanism

--- Eatstead.Infrastructure/Data/StoreContextSeed.cs
+++ Eatstead.Infrastructure/Data/StoreContextSeed.cs
@@ -1,17 +1,74 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Eatstead.Domain.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Valuegate.Infrastructure.Data;
 
 namespace Eatstead.Infrastructure.Data
 {
-    public class StoreContextSeed 
+    public class StoreContextSeed
     {
-        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        public static async Task SeedAsync(ApplicationDbContext context, ILoggerFactory loggerFactory)
         {
-            
+            try
+            {
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                if (!context.Menus.Any())
+                {
+                    using var transaction = context.Database.BeginTransaction();
+                    var menusData = File.ReadAllText("../Infrastructure/Data/SeedData/Menu.json");
+                    var menus = JsonSerializer.Deserialize<List<Menu>>(menusData);
+
+                    foreach (var item in menus)
+                    {
+                        context.Menus.Add(item);
+                    }
+
+                    ///   context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductBrands ON");
+
+                    await context.SaveChangesAsync();
+
+                    //    context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductBrands OFF");
+
+                    transaction.Commit();
+                }
+
+
+                if (!context.Cafeterias.Any())
+                {
+                    using var transaction = context.Database.BeginTransaction();
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/Cafeteria.json");
+                    var types = JsonSerializer.Deserialize<List<Cafeteria>>(typesData);
+
+                    foreach (var item in types)
+                    {
+                        context.Cafeterias.Add(item);
+                    }
+
+                    //    context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductTypes ON");
+
+                    await context.SaveChangesAsync();
+
+                    //   context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductTypes OFF");
+
+                    transaction.Commit();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
         }
-    }
+
+        }
+    
 }
