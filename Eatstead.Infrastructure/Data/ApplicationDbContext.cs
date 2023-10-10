78cc2b91f0aac79127c225df246ce7b609891eb2
donkennie <ajeigbekehinde160@gmail.com>
2023-10-10 10:08:41 +0100

    add seed data

--- Eatstead.Infrastructure/Data/ApplicationDbContext.cs
+++ Eatstead.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Eatstead.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Valuegate.Domain;
@@ -13,7 +16,18 @@ namespace Valuegate.Infrastructure.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+            
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Cafeteria> Cafeterias { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        
+
     }
 }
